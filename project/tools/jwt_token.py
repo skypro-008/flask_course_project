a0da32c9b353c4fcb08913b0e51f9be8c53539f7
Vadim Meshcheryakov <v.meshcheryakov@napoleonit.ru>
2021-11-29 10:44:29 +0500

    Надо еще немного тестов на jwt_token

---
+++ project/tools/jwt_token.py
@@ -0,0 +1,33 @@
+from calendar import timegm
+from datetime import datetime, timedelta
+from typing import Any, Dict
+
+import jwt
+from flask import current_app
+
+
+class JwtToken:
+
+    def __init__(self, data: Dict[str, Any]):
+        self._now = datetime.utcnow()
+        self._data = data
+        self._access_token_expiration = current_app.config['TOKEN_EXPIRE_MINUTES']
+        self._refresh_token_expiration = current_app.config['TOKEN_EXPIRE_DAYS']
+
+    def __get__token(self, time_delta: timedelta):
+        self._data.update({
+            'exp': timegm((self._now + time_delta).timetuple())
+        })
+        return jwt.encode(self._data, current_app.config['SECRET_KEY'], algorithm='HS256')
+
+    @property
+    def access_token(self) -> str:
+        return self.__get__token(timedelta(minutes=self._access_token_expiration))
+
+    @property
+    def refresh_token(self) -> str:
+        return self.__get__token(timedelta(days=self._refresh_token_expiration))
+
+    @staticmethod
+    def decode_token(token: str) -> Dict[str, Any]:
+        return jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
